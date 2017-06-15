c8fce4d78d3a02c3031c52632a4a720b52e220e7
archerLj <lj0011977@163.com>
2017-06-15 15:32:53 +0800

    init commit

---
+++ LFShare/LFShare.swift
@@ -0,0 +1,17 @@
+//
+//  TTTT.swift
+//  xxx
+//
+//  Created by archerLj on 2017/6/15.
+//  Copyright © 2017年 com.bocodo.csr. All rights reserved.
+//
+
+import Foundation
+
+public class LFShare {
+    public func sharePrint() {
+        print("come from share....")
+    }
+    
+    public init() {}
+}
